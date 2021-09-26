30bf2552070a1cc1cf9d65a3137f439f47db3fb0
DreDreDreDre <andrejar34@gmail.com>
2021-09-26 15:32:38 -0400

    Assignment2

--- src/main/java/ex37/App37.java
+++ src/main/java/ex37/App37.java
@@ -1,6 +1,5 @@
 package ex37;
 import java.util.*;
-import java.lang.String;
 /*
  *  UCF COP3330 Fall 2021 Assignment 2 Solution
  *  Copyright 2021 Andre Jara
@@ -8,54 +7,68 @@ import java.lang.String;
 
 public class App37
 {
-    public static double[] filterEvenNumbers(double[] nums)
-    {
-        int numEvens = 0;
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(nums[i] % 2 == 0)
-            {
-                numEvens++;
-            }
-        }
-
-        double[] evenNums = new double[numEvens];
-        int j = 0;
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(nums[i] % 2 == 0)
-            {
-                evenNums[j] = nums[i];
-                j++;
-            }
-        }
-        return evenNums;
-    }
     public static void main(String[] args)
     {
-        String numbers;
+        String password;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a list of numbers, separated by spaces: ");
-        numbers = sc.nextLine();
+        int sizeOfString;
+        int numSpecialChars;
+        int numNums;
+        int numChars;
+        int randomCharIdx;
+        int randomInt;
+        char specialCharacter;
+        char numCharacter;
+        Random rand = new Random();
+        List<Character> list = new ArrayList<Character>();
+        list.add('`'); list.add('~'); list.add('!'); list.add('@'); list.add('#'); list.add('$'); list.add('%'); list.add('^'); list.add('&'); list.add('*'); list.add('('); list.add(')'); list.add('-'); list.add('_'); list.add('+'); list.add('='); list.add('{'); list.add('['); list.add('}'); list.add(']'); list.add('|'); list.add('\\'); list.add(';'); list.add(':'); list.add('\''); list.add('\"'); list.add('<'); list.add('>'); list.add(','); list.add('.'); list.add('?'); list.add('/');
 
-        String[] array = numbers.split(" ");
-        double[] arrayNum = new double[array.length];
-        double[] evenNums;
-        for(int i = 0; i < array.length; i++)
+        System.out.print("What's the minimum length? ");
+        sizeOfString = sc.nextInt();
+
+        char[] string = new char[sizeOfString];
+
+        System.out.print("How many special characters? ");
+        numSpecialChars = sc.nextInt();
+
+        System.out.print("How many numbers? ");
+        numNums = sc.nextInt();
+
+        numChars = numSpecialChars + numNums;
+
+        for(int i = 0; i < numSpecialChars; i++)
         {
-            arrayNum[i] = Double.parseDouble(array[i]);
+            randomCharIdx = 0 + (int) (Math.random() * ((31 - 0) + 1));
+            string[i] = list.get(randomCharIdx);
         }
 
-        evenNums = filterEvenNumbers(arrayNum);
-
-        System.out.print("The even numbers are ");
-        for(int i = 0; i < evenNums.length; i++)
+        for(int i = numSpecialChars; i < numNums + numSpecialChars; i++)
         {
-            System.out.print(evenNums[i]);
-            if(i != evenNums.length - 1)
-            {
-                System.out.print(", ");
-            }
+            randomInt = 0 + (int) (Math.random() * ((9 - 0) + 1));
+            numCharacter = (char) (randomInt + '0');
+            string[i] = numCharacter;
         }
+
+        for(int i = numSpecialChars + numNums; i < string.length; i++)
+        {
+            string[i] = (char)(rand.nextInt(26) + 'a');
+        }
+
+        ArrayList<Character> charList = new ArrayList<Character>();
+
+        for(int i = 0; i < string.length; i++)
+        {
+            charList.add(string[i]);
+        }
+
+        Collections.shuffle(charList);
+
+        for(int i = 0; i < string.length; i++)
+        {
+            string[i] = charList.get(i);
+        }
+
+        password = new String(string);
+        System.out.println("Your password is " + password);
     }
 }
