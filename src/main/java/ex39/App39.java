4b394c34f526ac82eaea8fd16ab33ab774cd65d4
DreDreDreDre <andrejar34@gmail.com>
2021-09-27 19:28:36 -0400

    Assignment2

---
+++ src/main/java/ex39/App39.java
@@ -0,0 +1,14 @@
+package ex39;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Andre Jara
+ */
+
+public class App39
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/main/java/ex40/App40.java
@@ -0,0 +1,14 @@
+package ex40;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Andre Jara
+ */
+
+public class App40
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
